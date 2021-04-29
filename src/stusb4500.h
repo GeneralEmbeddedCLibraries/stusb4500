00c90b6c614522f62d8ae2ea7889a78614c2f678
ziga_miklosic <ziga.miklosic@gmail.com>
2021-04-29 07:40:59 +0200

    - change of comments/files style to be complient with Doxygen

--- src/stusb4500.h
+++ src/stusb4500.h
@@ -1,46 +1,56 @@
-//////////////////////////////////////////////////////////////
-// 
-//	project:		STUSB4500 EVALUATION
-//	date:			05.09.2020
-//	
-//	author:			Ziga Miklosic
-//
-//////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+/**
+*@file      stusb4500.h
+*@brief     Top level interface with USB PD chipset
+*@author    Ziga Miklosic
+*@date      29.04.2021
+*@version	V1.0.0
+*/
+////////////////////////////////////////////////////////////////////////////////
+/**
+*@addtogroup STUSB4500_API
+* @{ <!-- BEGIN GROUP -->
+*/
+////////////////////////////////////////////////////////////////////////////////
 
 #ifndef _STUSB4500_H_
 #define _STUSB4500_H_
 
-//////////////////////////////////////////////////////////////
-//	INCLUDES
-//////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+#include "project_config.h"
 #include "stdint.h"
 #include "stdbool.h"
 
-// For float
-#include "project_config.h"
+////////////////////////////////////////////////////////////////////////////////
+// Definitions
+////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////
-//	DEFINITIONS
-//////////////////////////////////////////////////////////////
-
-// Status
+/**
+ * 	General status
+ */
 typedef enum
 {
-	eSTUSB4500_OK 		= 0x00,
-	eSTUSB4500_ERROR 	= 0x01,
+	eSTUSB4500_OK 		= 0x00,		/**<Normal operation */
+	eSTUSB4500_ERROR 	= 0x01,		/**<General error state */
 } stusb4500_status_t;
 
-// PDO number
+/**
+ * 	PDO number
+ */
 typedef enum
 {
-	eSTUSB4500_PDO_1 = 0,
-	eSTUSB4500_PDO_2,
-	eSTUSB4500_PDO_3,
+	eSTUSB4500_PDO_1 = 0,	/**<PDO 1 selection */
+	eSTUSB4500_PDO_2,		/**<PDO 2 selection */
+	eSTUSB4500_PDO_3,		/**<PDO 3 selection */
 
 	eSTUSB4500_PDO_NUM_OF,
 } stusb4500_pdo_num_t;
 
-// Role swap
+/**
+ * 	Role swap
+ */
 typedef enum
 {
 	eSTUSB4500_ROLE_SWAP_NOT_SUPPORTED = 0,
@@ -49,7 +59,9 @@ typedef enum
 	eSTUSB4500_ROLE_SWAP_3_5A_5V,
 } stusb4500_role_swap_t;
 
-// PDO type
+/**
+ * PDO type
+ */
 typedef enum
 {
 	eSTUSB4500_PDO_TYPE_FIXED_SUPPLY = 0,
@@ -58,7 +70,9 @@ typedef enum
 	eSTUSB4500_PDO_TYPE_RESERVED,
 } stusb4500_pdo_type_t;
 
-// PDO flags
+/**
+ *  PDO flags
+ */
 typedef struct
 {
 	uint8_t dual_role_data 		: 1;
@@ -69,7 +83,9 @@ typedef struct
 	uint8_t reserved			: 3;
 } stusb4500_pdo_flags_t;
 
-// Fixed supply PDO (Power Delivery Object)
+/**
+ * 	Fixed supply PDO (Power Delivery Objects)
+ */
 typedef struct
 {
 	float32_t 					current;
@@ -80,14 +96,18 @@ typedef struct
 	stusb4500_pdo_num_t 		num;
 } stusb4500_pdo_t;
 
-// Attached mode
+/**
+ * 	Attached mode
+ */
 typedef enum
 {
 	eSTUSB4500_RDO_MODE_USB_C = 0,
 	eSTUSB4500_RDO_MODE_USB_PD
 } stusb4500_rdo_mode_t;
 
-// RDO (Requested Delivery Object)
+/**
+ *  RDO (Requested Delivery Object) data
+ */
 typedef struct
 {
 	struct
@@ -100,14 +120,18 @@ typedef struct
 	uint8_t 				src_pdo;
 } stusb4500_rdo_t;
 
-// Device attached
+/**
+ * 	Device attached flag
+ */
 typedef enum
 {
 	eSTUSB4500_NOT_ATTACHED = 0,
 	eSTUSB4500_ATTACHED,
 } stusb4500_attach_t;
 
-// CC status
+/**
+ * 	CC status
+ */
 typedef enum
 {
 	eSTUSB4500_CC_STAT_RESERVED = 0,
@@ -116,7 +140,9 @@ typedef enum
 	eSTUSB4500_CC_STAT_PWR_3_0,
 } stusb4500_cc_stat_t;
 
-// Status
+/**
+ *  Status overview
+ */
 typedef struct
 {
 	stusb4500_rdo_t 	RDO;
@@ -126,20 +152,12 @@ typedef struct
 } stusb4500_usb_status_t;
 
 
-//////////////////////////////////////////////////////////////
-//	VARIABLES
-//////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////
-// FUNCTIONS PROTOTYPES
-//////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Functions Prototypes
+////////////////////////////////////////////////////////////////////////////////
 stusb4500_status_t 				stusb4500_init			(void);
 stusb4500_status_t				stusb4500_hndl			(void);
 const stusb4500_usb_status_t * 	stusb4500_get_status	(void);
 
-
-//////////////////////////////////////////////////////////////
-// END OF FILE
-//////////////////////////////////////////////////////////////
 
 #endif // _STUSB4500_H_
